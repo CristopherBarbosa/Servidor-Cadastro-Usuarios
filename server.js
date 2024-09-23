e7ddf81b4e7d5d0a608fa42011efee2dfb5c51d1
CristopherBarbosa <cristopherbarbosa07@gmail.com>
2024-09-23 20:44:59 -0300

    params

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
+
+app.use(express.json())
+
+app.get('/usuarios/:id', (request, response) =>{
+    response.send("Olá, rota acessada com sucesso")
+} )
+
+app.post('/usuarios', (request, response) => {
+    response.send("Olá")
+})
 
 app.listen(3000)
