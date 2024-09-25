ef9ea36ccac06f9b712ba4bcf1303c1a405c55c4
CristopherBarbosa <cristopherbarbosa07@gmail.com>
2024-09-25 19:56:55 -0300

    get-and-post

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 import express, { request, response } from 'express'
+import { PrismaClient } from '@prisma/client'
+import { message } from 'statuses'
+
+const prisma = new PrismaClient()
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/usuarios/:id', (request, response) =>{
-    response.send("Olá, rota acessada com sucesso")
+app.get('/usuarios/:id', async (request, response) =>{
+    const users = await prisma.user.findMany()
+
+    response.status(200).json(users)
 } )
 
-app.post('/usuarios', (request, response) => {
-    response.send("Olá")
+app.post('/usuarios', async (request, response) => {
+     const users = await prisma.user.create({
+        data:{
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name
+        }
+    })
+
+    response.status(201).json({message: "Usuário criado com sucesso"})
 })
 
 app.listen(3000)
